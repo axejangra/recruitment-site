1369dd4ecfdf94a1d5420a9cf9019b1510ddc678
sahil <jangras777@gmail.com>
2023-07-07 15:11:07 +0530

    added homepage and login page

---
+++ index.js
@@ -0,0 +1,27 @@
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('slideanimationup');
+      }else{
+          entry.target.classList.remove('slideanimationup')
+
+      }
+    });
+  });
+const observer2 = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('slideanimationleft');
+      }
+    });
+  });
+
+  
+  observer.observe(document.querySelector('.benefits'));
+  observer2.observe(document.querySelector('.featureSection'));
